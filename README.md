2d70c142250d5e1747d01f2d7fa9455268d20a73
SebastianR0589 <sebastian.riedel0589@gmail.com>
2025-10-21 15:06:12 +0200

    refactoring, cors added, readme added

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Express_API_Sample
+# Sebastian Riedel Portfolio
 
+Welcome to my portfolio! Here you'll find some of the web projects I've built.
+
+---
+
+### Express.js API sample startups
+**Description:**  
+This project is a simple Express.js API which provides drifferent startups data depending on the Jquery parameters the user enters.
+
+
+**GitHub Repo:** [Link to repo](https://github.com/SebastianR0589/Express_API_Sample)
+
+**Screenshots:**
+![Screenshot 1](./screenshots/server.PNG)
+![Screenshot 2](./screenshots/controller.PNG)
+![Screenshot 3](./screenshots/routes.PNG)
+
+**Features:**
+- Fetch user search parameter
+- Give back data based upon search parameters
+
+**Tech Stack:**  
+Express.js
--- server.js
+++ server.js
@@ -1,67 +1,19 @@
 import express from 'express'
-import { startups } from './data/data';
-
-const PORT = 3000;
-const app = express();
-
-app.get('/api', (req, res) => {  
-let filteredData = startups
-
-  const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
-
-  if (industry) {
-    filteredData = filteredData.filter( startup => 
-      startup.industry.toLowerCase() === industry.toLowerCase()
-    )
-  }
-
-  if (country) {
-    filteredData = filteredData.filter( startup => 
-      startup.country.toLowerCase() === country.toLowerCase()
-    )
-  }
-  
-  if (continent) {
-    filteredData = filteredData.filter( startup => 
-      startup.continent.toLowerCase() === continent.toLowerCase()
-    )
-  }
-
-  if (is_seeking_funding) {
-    filteredData = filteredData.filter( startup => 
-      startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
-    )
-  }
-  
-  if (has_mvp) {
-    filteredData = filteredData.filter( startup => 
-      startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
-    )
-  }
+import cors from 'cors'
+import { apiRouter } from './routes/apiRoutes.js'
 
 
-  res.json(filteredData)
+const PORT = 8000
+
+const app = express()
+
+app.use(cors())
+
+app.use('/api', apiRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Endpoint not found. Please check the API documentation." })
 })
 
 
-app.get('/api/:field/:term', (req, res) => {
-  
-  const { field, term } = req.params
-
-    const allowedFields = ['country', 'continent', 'industry']
-
-
-  if (!allowedFields.includes(field)) {
-    return res.status(400).json({message: "Search field not allowed. Please use only 'country', 'continent', 'industry'" })
-  }
-
-
-  const filteredData = startups.filter(
-    startup => startup[field].toLowerCase() === term.toLowerCase()
-  )
-
-  res.json(filteredData)
-
-})
-
-app.listen(PORT, () => )
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
